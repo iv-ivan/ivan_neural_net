f38afcc81770644d6d7d809ecefea628e613e029
Ivan <ivan290895@yandex.ru>
2019-06-17 21:13:53 +0300

    Initial Commit

---
+++ NeuralNetwork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NeuralNetwork
+//
+//  Created by Ivan Ivashkovskii on 17/06/2019.
+//  Copyright © 2019 Ivan Ivashkovskii. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
